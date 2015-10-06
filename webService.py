e4a18efde001f6f6fa00bb123ef38c77f453c05a
sjrustle <sjasonrussell@gmail.com>
2015-10-06 11:20:43 -0400

    client and webservices

--- webService.py
+++ webService.py
@@ -1,11 +1,14 @@
 __author__ = 'Scott'
 import web
+import ebayFinder
 from soaplib.wsgi_soap import SimpleWSGISoapApp
 from soaplib.service import soapmethod
 from soaplib.serializers import primitive as soap_types
 
+
 urls = ("/ebay", "EbayServ",
         "/ebay.wsdl", "EbayServ",
+        "/","Hello"
         )
 render = web.template.Template("$def with (var)\n$:var")
 
@@ -13,6 +16,7 @@ class SoapService(SimpleWSGISoapApp):
     """Class for webservice """
     @soapmethod(soap_types.String, _returns=soap_types.String)
     def ebay_service(self,keyword):
+        ebayFinder.get_item()
         return keyword
 
 class EbayServ(SoapService):
@@ -22,14 +26,14 @@ class EbayServ(SoapService):
         for header, value in headers:
             web.header(header, value)
 
-    def GET(self):
-        response = super(SimpleWSGISoapApp, self).__call__(web.ctx.environ, self.start_response)
-        return render("\n".join(response))
-
-
     def POST(self):
         response = super(SimpleWSGISoapApp, self).__call__(web.ctx.environ, self.start_response)
         return render("\n".join(response))
+
+class Hello():
+    def GET(self):
+        return render
+
 
 app=web.application(urls, globals())
 
--- wsClient.py
+++ wsClient.py
@@ -1,1 +1,5 @@
 __author__ = 'Scott'
+from soaplib.client import make_service_client
+from webService import EbayServ
+client = make_service_client('http://localhost:8080/ebay',EbayServ())
+print client.ebay_service('iPhone')
