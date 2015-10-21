c7930a723b319315e956dd5345e2715061762b82
Scott <sjasonrussell@gmail.com>
2015-10-21 16:10:28 -0400

    Added a fail option

--- Auth_Ebay.py
+++ Auth_Ebay.py
@@ -4,6 +4,8 @@ import subprocess
 def authorized_user():
     if subprocess.call(["kinit"]) == 0:
         print "That worked"
+    else:
+        print "Fail"
     print "The Function Ran"
 
 authorized_user()
