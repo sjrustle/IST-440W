697cea55318da63ad27f2309f1f83c5b9f7474f0
Scott <sjasonrussell@gmail.com>
2015-10-25 17:44:56 -0400

    Json_web_tokens for encrytion and sending, Kerb_auth_ for checking auth, SQLConnection needs anme changed, webService needs to be refactored to look more consistent and readable, same goes for wsClient

--- wsClient.py
+++ wsClient.py
@@ -3,6 +3,7 @@ import getpass
 from soaplib.client import make_service_client
 from webService import EbayServ
 
+#client
 client = make_service_client('http://localhost:8080/ebay',EbayServ())
 user_name = raw_input("> Enter user: ")
 blatant_password = getpass.getpass("> Enter your password: ")
