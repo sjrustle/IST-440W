1fccb821578501ce9e57f37b2f6a7b9b88b87bb5
Scott <sjasonrussell@gmail.com>
2015-11-17 18:25:58 -0500

    Makes encoded string and return it

--- Create_Token.py
+++ Create_Token.py
@@ -4,3 +4,6 @@ import jwt
 # Returns JWT string
 def return_jwt(username):
     con = create_connection()
+    service = get_user(con, username)
+    encoded = jwt.encode({'user_service': service},'secret', algorithm='HS256')
+    return encoded
