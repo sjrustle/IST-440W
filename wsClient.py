3b2dfbf12a1dfddcb3937d9d04cb3072fb5f3e58
sjrustle <sjasonrussell@gmail.com>
2015-11-24 19:18:47 -0500

    Added try catch and json checking

--- wsClient.py
+++ wsClient.py
@@ -7,15 +7,20 @@ from webService import EbayServ
 #client
 try:
     client = make_service_client('http://localhost:8080/ebay',EbayServ())
-except:
-    print "Client failed"
 
-try:
     # Try to connect to client and get json token
     json = client.service_login("bkt5031", "H464Jd$")
 
     # Json sent back, with permissions decode
     decode = jwt.decode(json,'secret', algorithm='HS256')
+
     # Get message from rabbitmq
+    if decode.value(0) == 1:
+        # Has permissions for ebay, will do the ebay call here
+        print "User has permission"
+    else:
+        # User does not have permission
+        print "User does not have permission"
+
 except:
     print "Login Failed"
