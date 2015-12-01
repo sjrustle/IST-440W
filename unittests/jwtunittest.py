9b1fadcf872373b96a8e1ba0a3500378bdeb3d2f
sjrustle <sjasonrussell@gmail.com>
2015-12-01 10:01:16 -0500

    Added a con string test and json token receive

---
+++ unittests/jwtunittest.py
@@ -0,0 +1,10 @@
+import Create_Token
+import unittest
+
+class JsonTokenUnitTest(unittest):
+
+    def con_create_json(self):
+        con = Create_Token.create_connection()
+        self.assertEqual(con,)
+
+    def json_ticket_return_test(self):
