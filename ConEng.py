b66bce8cbf9b4d8f44a9480fdf5778e12de66b77
sjrustle <sjasonrussell@gmail.com>
2015-12-08 10:15:59 -0500

    ConEngine returns a string with forumla

--- ConEng.py
+++ ConEng.py
@@ -9,6 +9,7 @@ import math
 
 price_array = []
 date_array = []
+point_array = []
 
 
 
@@ -16,12 +17,12 @@ def itemFinder(search_item):
     items = []
     try:
         api = Finding(appid="ScottRus-bf7d-437a-a830-3735324dd553",config_file=None,debug=True)
-        for page_num in range(20):
+        for page_num in range(30):
             # TODO: Change back to range later
             response = api.execute('findCompletedItems', {'keywords': search_item,'paginationInput': {'entriesPerPage': 100, 'pageNumber':page_num}})
             response_dict = response.dict()
             if response_dict['ack'] == 'Failure':
-                print "It was a failure"
+                continue
             else:
                 items.append(response.dict())
     except:
@@ -37,7 +38,6 @@ def itemFinder(search_item):
         try:
             for item in item_array:
                 count += 1
-                print count
                 # Get selling status
                 selling_status_price = item['sellingStatus']
                 selling_status_price = item['sellingStatus']
@@ -61,9 +61,50 @@ def itemFinder(search_item):
                 int_delta = int(delta.days)
                 # Store Date in date array
                 date_array.append(int_delta)
+                # Y X pair
+                point_tuple = (value,int_delta)
+                point_array.append(point_tuple)
         except:
             error_logging("ConEng", "Failed")
-    return items
+
+
+def compute_error_for_line_give_points(b,m,points):
+    try:
+        totalError = 0
+        for i in range(0,len(points)):
+            y,x = points[i]
+            totalError += (y - (m * x + b)) ** 2
+        return totalError/float(len(points))
+    except:
+        error_logging("webService", "Error in computer error")
+
+def step_gradient(b_current, m_current, points, learningRate):
+    try:
+        b_gradient = 0
+        m_gradient = 0
+        N = float(len(points))
+        for i in range(0, len(points)):
+            y,x = points[i]
+            b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
+            m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
+        new_b = b_current - (learningRate * b_gradient)
+        new_m = m_current - (learningRate * m_gradient)
+        return [new_b, new_m]
+    except:
+        error_logging("webService", "Error in computer error")
+
+def runtest (search_item):
+    error = None
+    new_b, new_m = None, None
+    itemFinder(search_item)
+    (m,b) = pylab.polyfit(date_array,price_array,1)
+    for i in range(100):
+        new_b, new_m = step_gradient(b,m,point_array,.1)
+        error = compute_error_for_line_give_points(new_b,new_m,point_array)
+    return ("The new b{0}, the new m {1}, the error {2}".format(new_b,new_m,error))
+
+runtest("Xbox One")
+
 
 
 # itemFinder("iPhone")
