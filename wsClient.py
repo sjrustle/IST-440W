59001765382bad1c2cb927bced81d502d088093b
sjrustle <sjasonrussell@gmail.com>
2015-12-08 23:00:49 -0500

    Commented out decode lines in wsClient

--- wsClient.py
+++ wsClient.py
@@ -9,14 +9,14 @@ try:
     client = make_service_client('http://localhost:8080/ebay',EbayServ())
 
     # Try to connect to client and get json token
-    json = client.service_login("bkt5031", "H464Jd$", "PlayStation")
+    json = client.service_login("bkt5031", "H464Jd$","Play Station")
     print json
     # Json sent back, with permissions decode
-    decode = jwt.decode(json,'secret', algorithm='HS256')
-    print decode
-    print type(decode)
-    service_type = decode.get('user_service')
-    print service_type
+    # decode = jwt.decode(json,'secret', algorithm='HS256')
+    # print decode
+    # print type(decode)
+    # service_type = decode.get('user_service')
+    # print service_type
 
     # Get message from rabbitmq
     # if decode.value(0) == 1:
