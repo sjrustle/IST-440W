b293ce5362506870083eaaccf4d093c2dda40306
sjrustle <sjasonrussell@gmail.com>
2015-12-10 01:49:40 -0500

    Removed the connection method and made the variable global

--- rabbit_send.py
+++ rabbit_send.py
@@ -1,25 +1,22 @@
 import pika
 from wsLogging import error_logging, audit_logging
 
-def create_connection(queueName):
+try:
+    # Creates connection we can specify an ipAddress later
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+
+    # Sets up connection
+    channel = connection.channel("FirstQ")
+except:
+    error_logging("RabbitSend","Couldn't Create connection")
+
+def send_to(Message):
     try:
-        # Creates connection we can specify an ipAddress later
-        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-
-        # Sets up connection
-        channel = connection.channel(queueName)
-
-        return channel
-    except:
-        error_logging("RabbitSend","Couldn't Create connection")
-
-def send_to(qName,Message):
-    try:
-        channel = create_connection(qName)
         channel.basic_publish(exchange='',
-                      routing_key=qName,
+                      routing_key="FirstQ",
                       body=Message)
         audit_logging("RabbitSend","Sent to queue")
+        connection.close()
     except:
         error_logging("RabbitSend","Couldn't Send")
 
