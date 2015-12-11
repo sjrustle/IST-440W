2ad810fbaba5f052ae4e029dea552082a453d711
sjrustle <sjasonrussell@gmail.com>
2015-12-11 13:03:28 -0500

    RabbitMQ returns a file body, and wsClient has condidtion to run it or not

--- rabbit_receive.py
+++ rabbit_receive.py
@@ -11,13 +11,23 @@ channel.queue_declare(queue='FirstQ')
 print('[*] Waiting for messages. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
+    # Set up the textfile
+    try:
+        file_body = body.split()
+        _file = open("RabbitMq", "w")
+        _file.write(file_body)
+        _file.close()
+        return body
+    except:
+         error_logging("Rabbit Receive", "Error creating body")
+
 
 try:
     channel.basic_consume(callback,
                           queue='FirstQ',
                           no_ack=True)
+    audit_logging("Rabbit Receive", "Was able to receive")
 except:
-    error_logging("Rabbit Receive", "Error running CallBack")
+    error_logging("Rabbit Receive", "Error consuming")
 
 channel.start_consuming()
