9e7326871d66713d228e14235e276369a287049b
sjrustle <sjasonrussell@gmail.com>
2015-12-11 20:38:40 -0500

    Modified tests

--- unittests/test_con_eng.py
+++ unittests/test_con_eng.py
@@ -7,18 +7,18 @@ import ConEng
 class TestKerbAuthCheck(unittest.TestCase):
     def test_item_finder(self):
         con = ConEng.itemFinder("pillows",2)
-        self.assertTrue(con)
+        self.assertEqual(con,None)
 
     def test_error_compute(self):
         compute = ConEng.compute_error_for_line_give_points(1,1)
         self.assertTrue(compute)
 
     def test_step_grad(self):
-        step = ConEng.step_gradient(1,1,10,.01)
-        self.assertTrue(step)
+        step = ConEng.step_gradient(1,1,2,.01)
+        self.assertEqual(step,None)
 
     def test_run(self):
-        run = ConEng.runtest('iPhone',10,360)
+        run = ConEng.runtest('iPhone',2,360)
         self.assertTrue(run)
 
 
